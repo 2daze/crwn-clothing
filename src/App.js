db2077d3e5d8dc4417c45b8a984b3bd9c93fa66e
Abner Doubleday <abnerdaviddoubleday@hotmail.com>
2021-03-01 00:22:29 -0800

    new header with links to existing pages.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
 import HatsPage from './pages/HatsPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
-      <Route exact path='/shop/hats' component={HatsPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
